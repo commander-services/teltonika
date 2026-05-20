36525c32f9672815020ae348353afc8d5af72410
Peter Bartos <peter.bartos@commander.sk>
2026-05-20 11:24:33 +0200

    V3TECH-2868 Improve IO Element decoding with fallback logic and custom error type

--- ioelements/errors.go
+++ ioelements/errors.go
@@ -15,17 +15,3 @@ func (e FallbackIOValueError) Error() string {
 func (e FallbackIOValueError) Unwrap() error {
 	return e.Err
 }
-
-//type UndefinedElementId struct {
-//	Model string
-//	AvlId uint16
-//	Err   error
-//}
-//
-//func (e UndefinedElementId) Error() string {
-//	return fmt.Sprintf("undefined io element for model %q avl id %d", e.Model, e.AvlId)
-//}
-//
-//func (e UndefinedElementId) Unwrap() error {
-//	return e.Err
-//}
